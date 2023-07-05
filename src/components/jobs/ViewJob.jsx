7ae7e5552f73dc92541e67d08e0f541557b6093c
soft9011111 <138549884+soft9011111@users.noreply.github.com>
2023-07-05 13:04:15 +0530

    Update ViewJob.jsx

--- src/components/jobs/ViewJob.jsx
+++ src/components/jobs/ViewJob.jsx
@@ -1,54 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';      
+import Table from 'react-bootstrap/Table';
 import { createClient } from "@supabase/supabase-js";
-import view from '../../asset/view1.png'
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const supabase = createClient("https://jnklmllknvssnmqrtnue.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impua2xtbGxrbnZzc25tcXJ0bnVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyNzg3ODIsImV4cCI6MjAwMzg1NDc4Mn0.PJ6LXiLWtKVloHSjcSbe1gVnT9hTCR2OlaqsZXvIXh0");
 
-function ViewJob(id) {
+function ViewJob() {
   const navigate = useNavigate();
-  const [Jobs, setJobs] = useState([]);
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("job_id"))
+  const [JobDetails, setJobDetails] = useState([]);
 
   useEffect(() => {
-    getJobs();
+    getJobDetails();
   }, []);
 
-  async function getJobs() {
-    const { data } = await supabase.from("jobs").select();
-    setJobs(data);
+  async function getJobDetails() {
+    const { data, error } = await supabase.from("jobs")
+      .select()
+      .eq('id', searchParams.get("job_id"));
+    setJobDetails(data);
   }
-  
-  function viewjob () {
-    navigate('/viewjob');
-}
+
   return (
     <div className="listjobs">
       <Container>
-        <div className="addjobbtn"><Button style={{background:'gray', borderRadius:50}} variant="primary" href="/addjob" >Add Job</Button> 
-        </div>
         <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th width="50%">Client Name</th>
-              <th width="40%">Veh No</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {Jobs.map((job) => (
-              <tr>
-                <td>{job.client_name}
-                </td>
-                <td>{job.vehicle_no}
-                </td>
-                <td><img style={{ width: 40, height: 40 }} src={view}  alt="Logo" onClick={viewjob}/>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          {JobDetails.map((JobDetail) => (
+            <tbody>
+              <tr><td>Client Name</td><td>{JobDetail.client_name}</td></tr>
+              <tr><td>Vehicle No</td><td>{JobDetail.vehicle_no} </td></tr>
+              <tr><td>Job Type</td><td>{JobDetail.job_type} </td></tr>
+              <tr><td>Completion Date</td><td>{JobDetail.completion_date} </td> </tr>
+            </tbody>
+          ))}
         </Table>
       </Container>
     </div>);
