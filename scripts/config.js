ec4f0c021133f4134bcb42b8c09af513c66c312a
soft9011111 <138549884+soft9011111@users.noreply.github.com>
2023-07-10 22:49:27 +0530

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,6 @@
+const Config={
+    SUPABASE_URL : "https://jnklmllknvssnmqrtnue.supabase.co",
+    SUPABASE_KEY : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impua2xtbGxrbnZzc25tcXJ0bnVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyNzg3ODIsImV4cCI6MjAwMzg1NDc4Mn0.PJ6LXiLWtKVloHSjcSbe1gVnT9hTCR2OlaqsZXvIXh0",
+};
+
+export default Config
