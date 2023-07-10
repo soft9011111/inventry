29be0adc0d25b50b79ba57a2485c3bf2272954a2
soft9011111 <138549884+soft9011111@users.noreply.github.com>
2023-07-10 22:53:43 +0530

    Add files via upload

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,16 +1,6 @@
 const Config={
-    ROSBRIDGE_SERVER_IP: "192.168.43.119",
-    ROSBRIDGE_SERVER_PORT: "9090",
-    TOPIC_CMD_VEL: "/cmd_vel",
-    TOPIC_ARM_CMD_VEL: "/arm_cmd_vel",
-
-    MSG_TWIST: "geometry_msgs/Twist",
-
-    TOPIC_IMAGE: "/camera1/image_raw/compressed",
-    MSG_IMAGE: "sensor_msgs/msg/CompressedImage",
-
-    TOPIC_MAP: "/map",
-    MSG_MAP: "nav_msgs/msg/OccupancyGrid"
+    SUPABASE_URL : "https://jnklmllknvssnmqrtnue.supabase.co",
+    SUPABASE_KEY : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impua2xtbGxrbnZzc25tcXJ0bnVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyNzg3ODIsImV4cCI6MjAwMzg1NDc4Mn0.PJ6LXiLWtKVloHSjcSbe1gVnT9hTCR2OlaqsZXvIXh0",
 };
 
 export default Config
